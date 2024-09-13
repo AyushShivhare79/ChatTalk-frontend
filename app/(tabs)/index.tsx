f4d264bdd54f46353d9b924b2c2857539c4eda3c
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-09-13 17:35:18 +0530

    feat: chat working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,52 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, SafeAreaView, TextInput, Pressable } from "react-native";
 
 export default function HomeScreen() {
+  const [text, setText] = useState<string>();
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [newMessage, setNewMessage] = useState<string>("NOT");
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://10.0.2.2:8585");
+
+    console.log("Socket: ", socket);
+
+    socket.onopen = () => {
+      console.log("Connection");
+      setSocket(socket);
+    };
+
+    socket.onmessage = (message) => {
+      console.log("Message received:", message.data);
+      setNewMessage(message.data);
+    };
+
+    socket.onclose = () => {
+      console.log("CLOSED");
+    };
+    socket.onerror = (e: any) => {
+      console.log("Error");
+      console.log(e.message);
+    };
+  }, []);
   return (
-    <View className="flex justify-center items-center h-screen">
-      <Text>Hello Ayush</Text>
-    </View>
+    <SafeAreaView className="flex justify-center items-center h-screen">
+      <Text>This: {newMessage}</Text>
+      <TextInput
+        className="border border-black rounded-2xl w-full"
+        placeholder=" Type here..."
+        onChangeText={setText}
+        value={text}
+      />
+      <Pressable
+        className="flex justify-center items-center bg-black rounded-xl w-12 h-8"
+        onPress={() => {
+          socket?.send(text || "");
+          return setText("");
+        }}
+      >
+        <Text className="text-white">Send</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
